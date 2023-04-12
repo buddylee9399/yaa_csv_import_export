90878e2480367ae86229387f059aca9b870eea88
Jose Merconchini <buddylee939@gmail.com>
2023-04-12 16:35:14 -0400

    Adds better errors and binding of caller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#index'
+  # root to: 'static_pages#index'
   
   get   'about', to: 'static_pages#about'
   get   'contact', to: 'static_pages#contact'
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  # root "users#index"
 end
