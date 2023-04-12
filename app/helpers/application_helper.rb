f93969af0b19e116703a61e18a1bfbc88346069c
Jose Merconchini <buddylee939@gmail.com>
2023-04-12 16:35:43 -0400

    Added a navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+
+def menu_items
+  [{
+    name: 'Home',
+    path: root_path,
+  }, {
+    name: 'About',
+    path: about_path,
+  }, {
+    name: 'Contact',
+    path: contact_path,
+  }, {
+    name: 'Privacy',
+    path: privacy_path,
+  },].map do |item|
+    {
+      name: item[:name],
+      path: item[:path],
+      active: current_page?(item[:path])
+    }
+  end
+end
+
 end
