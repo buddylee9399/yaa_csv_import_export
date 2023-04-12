fd4d15c240ef51b907dcc55bc700d2a11a86fd2b
Jose Merconchini <buddylee939@gmail.com>
2023-04-12 16:32:11 -0400

    Added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#index'
+  
+  get   'about', to: 'static_pages#about'
+  get   'contact', to: 'static_pages#contact'
+  get   'privacy', to: 'static_pages#privacy'
   resources :posts
   resources :users do 
     collection {post :import}
